0e1fb6716f16fce5a591c17faf67321f3683e138
iago-silva-unvoid <iago.silva@unvoid.com>
2024-11-24 13:10:49 -0300

    Add bottom sections

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("here");
+document.addEventListener("DOMContentLoaded", () => {
+    const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+    const now = new Date();
+    const dayElement = document.querySelector("." + days[now.getDay()]);
+    dayElement === null || dayElement === void 0 ? void 0 : dayElement.classList.add("active");
+});
--- script.ts
+++ script.ts
@@ -1,1 +1,8 @@
-console.log("here")
+document.addEventListener("DOMContentLoaded", ()=> {
+	const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"]
+
+	const now = new Date()
+
+	const dayElement = document.querySelector("." + days[now.getDay()])
+	dayElement?.classList.add("active")
+})
